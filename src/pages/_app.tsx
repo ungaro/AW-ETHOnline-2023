822a40fa84f5081b8cdc5b88caa8ac73660536bd
Alp Guneysel <alp.guneysel@gmail.com>
2023-10-19 16:09:55 -0400

    add goerli

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,11 +18,16 @@ import {
   rainbowWallet,
 } from '@rainbow-me/rainbowkit/wallets'
 import { Chain } from '@rainbow-me/rainbowkit'
-import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains'
+import { mainnet, polygon, optimism, arbitrum, goerli } from 'wagmi/chains'
 import { createClient, configureChains, WagmiConfig } from 'wagmi'
 import { infuraProvider } from 'wagmi/providers/infura'
 import { publicProvider } from 'wagmi/providers/public'
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
+
+
+// CHAINS
+// Polygon, Scroll, 
+
 
 function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
@@ -37,41 +42,9 @@ function App({ Component, pageProps }: AppProps) {
 }
 export default App
 
-// Add Custom Chain
-// Demo purpose, gnosis is included in wagmi/chains
-// import { gnosis } from 'wagmi/chains'
-const gnosisChain: Chain = {
-  id: 100,
-  name: 'Gnosis',
-  network: 'gnosis',
-  iconUrl: 'https://uploads-ssl.webflow.com/63692bf32544bee8b1836ea6/637b0145cf7e15b7fbffd51a_favicon-256.png',
-  iconBackground: '#000',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'Gnosis',
-    symbol: 'xDAI',
-  },
-  rpcUrls: {
-    default: {
-      http: ['https://gnosischain-rpc.gateway.pokt.network'],
-    },
-  },
-  blockExplorers: {
-    etherscan: {
-      name: 'Gnosisscan',
-      url: 'https://gnosisscan.io/',
-    },
-    default: {
-      name: 'Gnosis Chain Explorer',
-      url: 'https://blockscout.com/xdai/mainnet/',
-    },
-  },
-  testnet: false,
-}
-
 // Web3 Configs
 const { chains, provider } = configureChains(
-  [mainnet, polygon, optimism, arbitrum, gnosisChain],
+  [mainnet, polygon, optimism, arbitrum, goerli],
   [
     infuraProvider({ apiKey: process.env.NEXT_PUBLIC_INFURA_ID !== '' && process.env.NEXT_PUBLIC_INFURA_ID }),
     jsonRpcProvider({
